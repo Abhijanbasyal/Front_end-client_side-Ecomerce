8c2d855a9b807b95408aff443f08b33935ca3d0b
PractiseSir <practisesir12@gmail.com>
2024-06-14 18:29:47 +0545

    Added the routes property

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div>
-      <Signup />
-      <Login />
+    <>
+      <Header />
+      <main>
 
-    </div>
-    
+        <Outlet />
+      </main>
+
+      <Footer />
+
+    </>
+
   );
 }
 
